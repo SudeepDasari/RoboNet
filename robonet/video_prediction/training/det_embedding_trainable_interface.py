3b6dd1c41ecc2cb74f4a625f67aeb71bfbabe187
febert <febert@berkeley.edu>
2019-06-28 18:47:57 -0700

    merged in ray

--- robonet/video_prediction/training/det_embedding_trainable_interface.py
+++ robonet/video_prediction/training/det_embedding_trainable_interface.py
@@ -5,14 +5,13 @@ class DetEmbeddingVPredTrainable(VPredTrainable):
 
     def _default_hparams(self):
         params = super()._default_hparams()
-        params.add_hparam('balance_camera_configurations', True)
-        params.set_hparam('batch_size', 32)
+        params.add_hparam('balanced_camera_configurations', True)
         return params
 
     def _filter_metadata(self, metadata):
         super()._filter_metadata(metadata)
 
-        if self._hparams.balance_camera_configurations:
+        if self._hparams.balanced_camera_configurations:
             assert self.dataset_hparams.get('sub_batch_size', 1) > 1
             unique_cameras = metadata['camera_configuration'].frame.unique().tolist()   # all camera configs that are in  he dataset
             all_metadata = metadata
