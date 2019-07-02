f493c716b4932249ce34ac151df91353cfdc0682
febert <febert@berkeley.edu>
2019-07-02 10:23:14 -0700

    sc

--- robonet/datasets/variants/combined_variants.py
+++
@@ -1,8 +0,0 @@
-from robonet.datasets.variants.held_out_robot_dataset import HeldoutRobotDataset
-from robonet.datasets.variants.annotation_benchmark_dataset import AnnotationBenchmarkDataset
-
-class AnnotationHeldoutRobotDataset(HeldoutRobotDataset, AnnotationBenchmarkDataset):
-    def train_val_filter(self, train_metadata, val_metadata):
-        train_metadata, val_metadata = HeldoutRobotDataset.train_val_filter(self, train_metadata, val_metadata)
-        train_metadata, val_metadata = AnnotationBenchmarkDataset.train_val_filter(self, train_metadata, val_metadata)
-        return train_metadata, val_metadata
