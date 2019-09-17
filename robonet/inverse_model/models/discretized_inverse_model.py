b71eea61d036e4034416679aed0110f232f6a03f
Sudeep Dasari <sdasari@andrew.cmu.edu>
2019-09-17 14:18:30 -0400

    fixed bug in action discretization. re-running training

--- robonet/inverse_model/models/discretized_inverse_model.py
+++ robonet/inverse_model/models/discretized_inverse_model.py
@@ -19,13 +19,22 @@ class DiscretizedInverseModel(BaseInverseModel):
             "pivots": [[-0.04483253140755173, -0.02947711320550581, -0.018373884708696702, -0.008892051974322548, -4.59881939272745e-05, 0.008815899693566963, 0.018292582474913204, 0.02938255920278165, 0.04470332342338521],
                         [-0.044674549010427486, -0.029352782231283018, -0.018263887904468375, -0.008836470630237072, 7.81874877900302e-06, 0.00884825636063618, 0.01830693463003378, 0.029377939442953, 0.04473508111072804],
                         [-0.10348141529525286, -0.06793363038544242, -0.042405628783200776, -0.02067683018449292, -0.0003540274691179853, 0.019988218195319766, 0.04168513725690283, 0.06726936589279635, 0.10260515613003221],
-                        [-0.22409500837108018, -0.1470685835529137, -0.09166876049855337, -0.04419968109806307, 5.580875190224738e-05, 0.044414223320168145, 0.09168509202611021, 0.1469321233733917, 0.2237400683241968]]
+                        [-0.22409500837108018, -0.1470685835529137, -0.09166876049855337, -0.04419968109806307, 5.580875190224738e-05, 0.044414223320168145, 0.09168509202611021, 0.1469321233733917, 0.2237400683241968]],
+            "means": [[-0.05844043352506317, -0.0365598888753108, -0.02371854361080623, -0.01355633452537272, -0.004447061217304071, 0.004359558603466982, 0.01346781084244209, 0.02363783086130393, 0.036456939880113295, 0.05834560772861528],
+                      [-0.05831025927528526, -0.03643373528153938, -0.023609139710274608, -0.013465667182953755, -0.004399357688117235, 0.004405043570967748, 0.013491056349448851, 0.023632353969085647, 0.03646405448080863, 0.0583175660974888],
+                      [-0.14210753817324154, -0.08433897448430323, -0.054693763651882464, -0.03133158710778195, -0.010471111756616646, 0.00976338713468559, 0.030621494148596932, 0.05401384675615853, 0.08356642563278535, 0.1406814351222195],
+                      [-0.30673709675244115, -0.1826234528754964, -0.11831810064105407, -0.06747048133665953, -0.02199376800432712, 0.02209506703978301, 0.06762712392804507, 0.11832652238765545, 0.18242774553595653, 0.30635348910031857]]
         }
 
     def _model_fn(self, model_inputs, model_targets, mode):
         inputs, targets = {}, None
         inputs['start_images'] = model_inputs['images'][:, 0]
         inputs['goal_images'] = model_inputs['images'][:, -1]
+
+        n_xy = (len(self._hparams.pivots[0]) + 1) * (len(self._hparams.pivots[1]) + 1)
+        n_z =  len(self._hparams.pivots[2]) + 1
+        n_theta = len(self._hparams.pivots[3]) + 1
+
         if mode == tf.estimator.ModeKeys.TRAIN:
             B = model_targets['actions'].get_shape().as_list()[0]
             inputs['T'] = model_targets['actions'].get_shape().as_list()[1]
@@ -36,13 +45,13 @@ class DiscretizedInverseModel(BaseInverseModel):
             binned_actions = []
             for a in range(input_adim):
                 binned_action = tf.zeros((B, inputs['T']), dtype=tf.int32)
-                for p in range(len(self._hparams.pivots)):
-                    binned_action = tf.where_v2(model_targets['actions'][:, :, a] > p, binned_action + 1, binned_action)
+                for p in range(len(self._hparams.pivots[a])):
+                    pivot = self._hparams.pivots[a][p]
+                    binned_action = tf.where_v2(model_targets['actions'][:, :, a] > pivot, binned_action + 1, binned_action)
                 binned_actions.append(binned_action)
-            xy_act = binned_actions[0] + (len(self._hparams.pivots[0]) + 1) + binned_actions[1]
-            n_xy = (len(self._hparams.pivots[0]) + 1) * (len(self._hparams.pivots[1]) + 1)
-            z_act, n_z = binned_actions[2], len(self._hparams.pivots[2]) + 1
-            theta_act, n_theta = binned_actions[3], len(self._hparams.pivots[3]) + 1
+            
+            xy_act = binned_actions[0] + (len(self._hparams.pivots[0]) + 1) * binned_actions[1]
+            z_act, theta_act = binned_actions[2], binned_actions[3]
             one_hot_actions = [tf.one_hot(tensor, n_dim) for tensor, n_dim in zip((xy_act, z_act, theta_act), (n_xy, n_z, n_theta))]
             inputs['real_actions'] = tf.concat(one_hot_actions, -1)
         else:
@@ -84,5 +93,12 @@ class DiscretizedInverseModel(BaseInverseModel):
             return est, scalar_summaries, {}
             
         #test
+        means = tf.convert_to_tensor(self._hparams.means)
+        pred_xy = outputs['pred_actions'][:, :, :n_xy]
+        pred_z = outputs['pred_actions'][:, :, n_xy:n_z + n_xy]
+        pred_theta = outputs['pred_actions'][:, :, n_z + n_xy:]
+        
+        import pdb; pdb.set_trace()
+        pred_xy = tf.reshape(tf.random.categorical(tf.reshape(pred_xy, (-1, n_xy)), 1, dtype=tf.int32), (-1, model_inputs['T']))
         return outputs['pred_actions']
 
